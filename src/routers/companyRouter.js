cc492bd4a8e9b27731c7d3824d3c1cc344547006
= <=>
2026-02-16 13:16:04 +0100

    Connexion + liens entre login et signin

--- src/routers/companyRouter.js
+++ src/routers/companyRouter.js
@@ -1,7 +1,10 @@
 import express from "express";
-import { getCompanySignin, postCompanySignin } from "../controllers/companyController.js";
+import { getCompanyLogin, getCompanySignin, postCompanyLogin, postCompanySignin } from "../controllers/companyController.js";
 
 export const companyRouter = express.Router();
 
 companyRouter.get("/signin", getCompanySignin);
 companyRouter.post("/signin", postCompanySignin);
+
+companyRouter.get("/login", getCompanyLogin);
+companyRouter.post("/login", postCompanyLogin);
