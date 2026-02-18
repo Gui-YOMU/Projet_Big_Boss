831491848e5079230771a74d018b20814511c0a1
= <=>
2026-02-18 12:25:21 +0100

    add mission ok

--- src/routers/missionRouter.js
+++ src/routers/missionRouter.js
@@ -1,4 +1,7 @@
 import express from "express";
 import { companyAuthguard } from "../services/companyAuthguard.js";
+import { giveMission } from "../controllers/missionController.js";
 
 export const missionRouter = express.Router();
+
+missionRouter.post("/add", companyAuthguard, giveMission)
